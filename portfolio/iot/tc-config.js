51045532ffc20ac14ae44840704742213ef882fd
Darius Cubbage <dariuscubbage@gmail.com>
2024-12-05 18:33:05 -0500

    created portfolio folder and upload AWS IoT eLearning

---
+++ portfolio/iot/tc-config.js
@@ -0,0 +1,20 @@
+/* Tin Can configuration */
+
+//
+// ActivityID that is sent for the statement's object
+//
+TC_COURSE_ID = "http://ZINW3ZxZzUZYlFEgHVFC4SnNt8XD7D-w_rise"
+
+//
+// CourseName for the activity
+//
+TC_COURSE_NAME = {
+  "en-US": "AWS IoT Device Management (300)"
+};
+
+//
+// CourseDesc for the activity
+//
+TC_COURSE_DESC = {
+  "en-US": "&lt;p&gt;AWS IoT Device Management makes it easy to securely on-board, organize, monitor, and remotely manage IoT Devices at scale. This course teaches you to use the features of AWS IoT Device Management in the AWS Management Console and AWS Command Line Interface (CLI), starting with provisioning of single or bulk IoT Devices. You learn to use thing groups, dynamic thing groups, fleet indexing, and search to organize IoT devices logically. Lastly, you learn to monitor and manage IoT devices using fine-grained logging and jobs. &lt;/p&gt;"
+};
